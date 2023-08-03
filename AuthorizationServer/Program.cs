2fdd99309f6ad2efca46c88d0440d778997168ba
Hassan Tariq <hassan.tariq@gmail.com>
2023-08-04 00:24:18 +0500

    Added ability to login and logout

--- AuthorizationServer/Program.cs
+++ AuthorizationServer/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+        {
+            options.LoginPath = "/account/login";
+        });
 
 var app = builder.Build();
 
@@ -12,13 +20,20 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication();
+
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
