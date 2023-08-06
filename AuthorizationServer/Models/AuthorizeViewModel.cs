eec39eb1e70550b48de1042544c85aef769b8a1e
Hassan Tariq <hassan.tariq@gmail.com>
2023-08-07 02:03:05 +0500

    Implemented explicty consent type.

---
+++ AuthorizationServer/Models/AuthorizeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AuthorizationServer.Models
+{
+    public class AuthorizeViewModel
+    {
+        [Display(Name = "Application")]
+        public string? ApplicationName { get; set; }
+
+        [Display(Name = "Scope")]
+        public string? Scope { get; set; }
+    }
+
+}
